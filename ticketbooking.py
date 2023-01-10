45e5776e92b03b09e811c14ef4cfa0210cc8f75a
indresh516 <92733684+indresh516@users.noreply.github.com>
2023-01-10 10:57:25 +0530

    Add files via upload

--- ticketbooking.py
+++ ticketbooking.py
@@ -22,22 +22,37 @@ class ticketinfo:
     def buy_ticket(self):
         buy_row=int(input("Enter the row number : "))
         buy_col=int(input("Enter the col number : "))
-        ticket_price=10
-        ch=int(input(f"You have choose row {buy_row} and col {buy_col}. the Price of this ticket is {ticket_price}\nAre you Sure You want to continue 1.Yes. 2. No  :  "))
+        self.ticket_price=10
+        ch=int(input(f"You have choose row {buy_row} and col {buy_col}. the Price of this ticket is {self.ticket_price}\nAre you Sure You want to continue 1.Yes. 2. No  :  "))
         if ch==1:
             name = input("Enter Your Name : ")
             Gender = input("Enter Your Gender : ")
             Age = int(input("Enter Your Age : "))
             phone_no = input("Enter Your Mobile No : ")
             TicketId = str(buy_row)+str(buy_col)
-            self.user_detail[TicketId]=[name,Gender,Age,phone_no]
+            self.user_detail[TicketId]=[name,Gender,Age,phone_no,self.ticket_price]
             print("Ticket booked Successfully..........")
             print(self.user_detail)
         else:
             print("User Aborted Ticket booking. No ticket booked...")
 
+    def statistics(self):
+        no_of_ticket_booked=len(self.user_detail)
+        total_seats = self.rows * self.cols
+        percentage_booked_seats= (no_of_ticket_booked/total_seats)*100
 
+        all_ticket_price=[]
+        for k,v in self.user_detail.items():
+            all_ticket_price.append(v[4])
+        current_income=sum(all_ticket_price)
+        total_income=total_seats*self.ticket_price
 
+        print("Number of Purchased Tickets : ",no_of_ticket_booked)
+        print(f"Percentage : {percentage_booked_seats}%")
+        print(f"Current income: ${current_income}")
+        print(f"Total income : ${total_income}")
+
+        
 
 
 
